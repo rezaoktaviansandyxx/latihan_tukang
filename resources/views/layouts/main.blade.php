a27a7093e3a60c3995ad5c0874857cbbda342410
stevenleonard271 <stevenleonard271@gmail.com>
2022-08-26 16:55:37 +0700

    update view template using blade

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,13 @@
+@include('partials.header')
+@include('partials.sidebar')
+@include('partials.topbar')                
+               
+<!-- Begin Page Content -->
+<div class="container-fluid">
+ @yield('content')
+</div>
+<!-- /.container-fluid -->
+</div>
+<!-- End of Main Content -->
+@include('partials.footer')
+            
